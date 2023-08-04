3a9c07716229f6915dbe26d1a47f7f60e4ef6e51
Sathiadak <sathiadak@gmail.com>
2023-08-03 23:08:46 -0400

    Query_08

--- Query_08/Query_08.sql
+++ Query_08/Query_08.sql
@@ -1,6 +1,7 @@
+Select * from language;
 Select * from film;
 With language_id_table as (select language_id from language) 
-select *from film cross join language_id_table;
+select *from film cross join language_id_table Where film_id=384;
 
 
 
