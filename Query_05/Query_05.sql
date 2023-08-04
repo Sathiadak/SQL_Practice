d853135210b0bc93e5e3a2922c908830aa78124e
Sathiadak <sathiadak@gmail.com>
2023-08-03 20:05:36 -0400

    Query_05

---
+++ Query_05/Query_05.sql
@@ -0,0 +1,11 @@
+Select * from customer;
+select * from payment;
+select * from rental;
+
+select c.customer_id, c.first_name, c.last_name, r.rental_date,r.return_date,
+p.payment_id, p.payment_date,p.amount, max(p.amount) from customer as c 
+right join payment as p on p.customer_id=c.customer_id 
+left join rental as r on r.customer_id=c.customer_id 
+group by c.customer_id, c.first_name, c.last_name, r.rental_date,r.return_date,
+p.payment_id, p.payment_date,p.amount 
+having max(p.amount)>5.99 order by p.amount desc;   
