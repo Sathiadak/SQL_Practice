034b3e383f5425a452ffb1f478549d173ee82e80
Sathiadak <sathiadak@gmail.com>
2023-08-03 20:21:01 -0400

    Query_05

--- Query_05/Query_05.sql
+++ Query_05/Query_05.sql
@@ -6,6 +6,6 @@ select c.customer_id, c.first_name, c.last_name, r.rental_date,r.return_date,
 p.payment_id, p.payment_date,p.amount, max(p.amount) from customer as c 
 right join payment as p on p.customer_id=c.customer_id 
 left join rental as r on r.customer_id=c.customer_id 
-group by c.customer_id, c.first_name, c.last_name, r.rental_date,r.return_date,
+where c.customer_id=204 group by c.customer_id, c.first_name, c.last_name, r.rental_date,r.return_date,
 p.payment_id, p.payment_date,p.amount 
 having max(p.amount)>10.99 order by p.amount desc;   
